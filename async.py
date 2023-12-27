c2ea8952bad3f3d5e922edf96358176442506676
Binduguptaa <bindeshgupta144@gmail.com>
2023-12-27 14:45:03 +0530

    dont know

--- async.py
+++ async.py
@@ -1,1 +1,18 @@
 import asyncio
+
+async def hello():
+    i = 2
+    while True:
+        print('hello')
+        i = i + 1
+        if i < 8 :
+            break
+async def bye():
+    i = 2
+    while True:
+        print('bye')
+        i = i + 1
+        if i < 8 :
+            break
+await hello()
+await bye()
--- webserver.js
+++ webserver.js
@@ -7,6 +7,7 @@ const server = http.createServer((req, res) => {
 
 
 })
+
 server.listen(8000, '127.0.0.1', () => {
     console.log('server started!')
 })
