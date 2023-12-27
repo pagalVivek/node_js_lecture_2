9028edfa3d364ac09d782322cde6b0ae1b3602d0
Binduguptaa <bindeshgupta144@gmail.com>
2023-12-27 16:34:29 +0530

    !

--- webserver.js
+++ webserver.js
@@ -1,13 +1,36 @@
 const http = require('http')
 const fs = require('fs')
 const html = fs.readFileSync('./Template/index.html', 'utf-8')
+// create a server
 const server = http.createServer((req, res) => {
-    res.end(html);
+    let path = req.url 
+
+    if (path === '/' || path.toLocaleLowerCase() === '/home') {
+        res.end(html.replace('{{%CONTENT%}}', 'You are in home page!'))
+
+    }
+    else if (path.toLocaleLowerCase() === '/about') {
+        res.end(html.replace('{{%CONTENT%}}', 'You are in about page!'))
+    }
+
+    else if (path.toLocaleLowerCase() === '/product') {
+        res.end(html.replace('{{%CONTENT%}}', 'You are in product page!'))
+    }
+
+    else if (path.toLocaleLowerCase() === '/contact') {
+        res.end(html.replace('{{%CONTENT%}}', 'You are in contact page!'))
+    }
+    else {
+        res.end(('{{%CONTENT%}}', 'ERROR 404 => page not found!'))
+    }
+
+
     console.log("A new request received!");
 
 
 })
 
+// start a server
 server.listen(8000, '127.0.0.1', () => {
     console.log('server started!')
 })
